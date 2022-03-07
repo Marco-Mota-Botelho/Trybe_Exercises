5602f5fadf2fa27e5145c14625630d9f3842a684
Marco Botelho <marcobotelho93@gmail.com>
2022-03-07 16:39:39 -0300

    Made a switch to tell how to move each piece in chess

---
+++ Fundamentos/Bloco_4_Javascript/day-1-javascript-first-steps/exercise_6.js
@@ -0,0 +1,27 @@
+const piece = "PeÃO";
+
+switch (piece.toLowerCase()) {
+  case "bispo":
+    console.log("Diagonais");
+    break;
+  case "rainha":
+    console.log("Diagonais e Retas");
+    break;
+  case "rei":
+    console.log("Um passo em qualquer direção");
+    break;
+  case "torre":
+    console.log("Retas");
+    break;
+  case "cavalo":
+    console.log("Movimento em L");
+    break;
+  case "peão":
+    console.log(
+      "Se for o primeiro movimento, até 2 passos para frente. Se não somente 1. Captura somente nas diagonais!"
+    );
+    break;
+  default:
+    console.log("Peça não encontrada");
+    break;
+}
