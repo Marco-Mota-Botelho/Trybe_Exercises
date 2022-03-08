b94676616250bd27f151cdc31133291657e7c738
Marco Botelho <marcobotelho93@gmail.com>
2022-03-08 15:37:11 -0300

    Finds the largest number on the array

--- Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/exercise1-7.js
+++ Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/exercise1-7.js
@@ -1,6 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let i;
+let i
 let sumOfNumbers = 0;
+let comparator = numbers[0];
 
 // console.log(numbers);
 
@@ -12,6 +13,14 @@ for (i = 0; i < numbers.length; i++) {
 let avarageOfNumbers = sumOfNumbers / numbers.length;
 console.log(sumOfNumbers);
 
-if (avarageOfNumbers > 20){
-  console.log('Valor maior que 20');
-} else console.log('Valor menor ou igual a 20');
+if (avarageOfNumbers > 20) {
+  console.log("Valor maior que 20");
+} else console.log("Valor menor ou igual a 20");
+
+for(i= 1; i < numbers.length; i++){
+  if (comparator < numbers[i]){
+    comparator = numbers[i]
+  }
+}
+
+console.log(comparator);
