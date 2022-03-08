533213c2d6e8f03ad6fd19dfa67fa138bf0d68d1
Marco Botelho <marcobotelho93@gmail.com>
2022-03-08 15:43:29 -0300

    Finds how many odd numbers there are

--- Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/exercise1-7.js
+++ Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/exercise1-7.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let i
+let i;
+let howManyOdds = 0;
 let sumOfNumbers = 0;
 let comparator = numbers[0];
 
@@ -17,10 +18,19 @@ if (avarageOfNumbers > 20) {
   console.log("Valor maior que 20");
 } else console.log("Valor menor ou igual a 20");
 
-for(i= 1; i < numbers.length; i++){
-  if (comparator < numbers[i]){
-    comparator = numbers[i]
+for (i = 1; i < numbers.length; i++) {
+  if (comparator < numbers[i]) {
+    comparator = numbers[i];
   }
 }
+
+for (i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 != 0) {
+    howManyOdds++;
+  }
+}
+if (howManyOdds > 0) {
+  console.log(howManyOdds);
+} else console.log('nenhum valor Ímpar encontrado');
 
 console.log(comparator);
