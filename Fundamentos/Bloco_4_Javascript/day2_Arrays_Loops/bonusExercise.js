2b1d510e769e155723f50277d59c0119d10bf0c7
Marco Botelho <marcobotelho93@gmail.com>
2022-03-08 19:27:30 -0300

    Helping people with their questions

--- Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/bonusExercise.js
+++ Fundamentos/Bloco_4_Javascript/day2_Arrays_Loops/bonusExercise.js
@@ -1,17 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let secondNumbers = new Array;
+//let secondNumbers = new Array;
 
-// for (let index = 1; index < numbers.length; index += 1) {
-//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-//     if (numbers[index] < numbers[secondIndex]) {
-//       let position = numbers[index];
-//       numbers[index] = numbers[secondIndex];
-//       numbers[secondIndex] = position;
-//     }
-//   }
-// }
+console.log(numbers);
 
-// console.log(numbers);
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+console.log(numbers);
 
 // for (let index = 1; index < numbers.length; index += 1) {
 //   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
@@ -23,15 +24,15 @@ let secondNumbers = new Array;
 //   }
 // }
 
-for (let index = 0; index < numbers.length; index+= 1){
-    let newNumber;
-    if((numbers.length-1) <= index){
-        newNumber = numbers[index] * 2;
-        secondNumbers.push(newNumber);
-    } else
-    {newNumber = numbers[index] * numbers[index+1];
-    secondNumbers.push(newNumber)}
-}
+// for (let index = 0; index < numbers.length; index+= 1){
+//     let newNumber;
+//     if((numbers.length-1) <= index){
+//         newNumber = numbers[index] * 2;
+//         secondNumbers.push(newNumber);
+//     } else
+//     {newNumber = numbers[index] * numbers[index+1];
+//     secondNumbers.push(newNumber)}
+// }
 
-console.log(numbers);
-console.log(secondNumbers);
+// console.log(numbers);
+// console.log(secondNumbers);
