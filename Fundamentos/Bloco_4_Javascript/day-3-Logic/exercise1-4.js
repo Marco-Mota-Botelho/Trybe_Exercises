3bbade190ae68a2981a35d2d1c071909c8a1cf75
Marco Botelho <marcobotelho93@gmail.com>
2022-03-09 14:02:42 -0300

    The code can now find the smallest word in an array of words

--- Fundamentos/Bloco_4_Javascript/day-3-Logic/exercise1-4.js
+++ Fundamentos/Bloco_4_Javascript/day-3-Logic/exercise1-4.js
@@ -27,3 +27,22 @@ for (i = splitWord.length - 1; i >= splitWord.length / 2; i -= 1) {
 reverseWord = splitWord.join("");
 
 console.log(reverseWord);
+
+//Exercise 3 Return the biggest word in an array and the smallest
+
+let arrayOfWords = ["java", "javascript", "python", "html", "css"];
+let biggerWord = arrayOfWords[0];
+let smallerWord = arrayOfWords[0];
+
+for (let word of arrayOfWords) {
+  if (word.length > biggerWord.length) {
+    biggerWord = word;
+  }
+}
+for (let word of arrayOfWords) {
+  if (word.length < smallerWord.length) {
+    smallerWord = word;
+  }
+}
+console.log(biggerWord);
+console.log(smallerWord);
