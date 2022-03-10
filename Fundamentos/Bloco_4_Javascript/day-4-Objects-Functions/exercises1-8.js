9171b4577d816ad4b0c3a6a8b6f389c2308c197e
Marco Botelho <marcobotelho93@gmail.com>
2022-03-10 17:21:19 -0300

    I was playing around with maps to test myself

--- Fundamentos/Bloco_4_Javascript/day-4-Objects-Functions/exercises1-8.js
+++ Fundamentos/Bloco_4_Javascript/day-4-Objects-Functions/exercises1-8.js
@@ -1,19 +1,66 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+let infos = [
+  {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  },
+  {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O ultimo MacPatinhas",
+    recorrente: "Sim",
+  },
+];
 
-  console.log('Bem-vinda, ' + info.personagem);
+let perts = {
+  personagens: [],
+  origens: [],
+  notas: [],
+  recorrentes: [],
+};
 
-  info['recorrente'] = 'Sim';
+perts.personagens = infos.map((info) => info.personagem).join(" e ");
+perts.origens = infos.map((info) => info.origem).join(" e ");
+perts.notas = infos.map((info) => info.nota).join(" e ");
+perts.recorrentes = infos.map((info) => info.recorrente).join(" e ");
 
-  console.table(info);
+console.log(
+  perts.personagens,
+  "\n" + perts.origens,
+  "\n" + perts.notas,
+  "\n" + perts.recorrentes
+);
+// for (let info of infos) {
+//     console.log(info)
+//     perts.push(info.personagem)
+// }
 
-  for(let key in info){
-      console.log(key);
-  }
+// for(let info in infos){
+//     for(let key in info.keys){
+//         console.log(info[key[0]] + ' e ' + info[key]);
+//     }
+// }
+// console.log("Bem-vinda, " + info.personagem);
 
-  for(let key in info){
-    console.log(info[key]);
-}
+// console.table(info);
+
+// for (let key in info) {
+//   console.log(key);
+// }
+
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+
+// console.log(
+//   info[0].personagem +
+//     " e " +
+//     info[1].personagem +
+//     "\b" +
+//     info[0].origem +
+//     " e " +
+//     info[1].origin +
+//     "\b" +
+//     info[0].nota + ' e '
+// );
