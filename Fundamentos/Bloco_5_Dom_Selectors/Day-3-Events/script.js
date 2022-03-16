771d373f694f403b898014086c25e257d116b471
Marco Botelho <marcobotelho93@gmail.com>
2022-03-16 10:54:03 -0300

    created the Holiday button and fixed my month function

--- Fundamentos/Bloco_5_Dom_Selectors/Day-3-Events/script.js
+++ Fundamentos/Bloco_5_Dom_Selectors/Day-3-Events/script.js
@@ -9,12 +9,6 @@ function createDaysOfTheWeek() {
     "Sábado",
   ];
   const weekDaysList = document.querySelector(".week-days");
-  const monthDaysList = document.querySelector("#days");
-  const dezDaysList = [
-    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-  ];
-
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
     const dayListItem = document.createElement("li");
@@ -22,25 +16,46 @@ function createDaysOfTheWeek() {
 
     weekDaysList.appendChild(dayListItem);
   }
+}
 
-  for (let day of dezDaysList) {
-    const dayListItem = document.createElement("li");
-    dayListItem.className = "day";
-    dayListItem.innerText = day;
-
-    if (day === 24 || day === 25 || day === 31) {
-      dayListItem.className += " holiday";
-      if (day === 25) {
-        dayListItem.className += " friday";
+function createDaysOfTheMonth (){
+    const monthDaysList = document.querySelector("#days");
+    const dezDaysList = [
+        29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+      ];
+    for (let day of dezDaysList) {
+        const dayListItem = document.createElement("li");
+        dayListItem.className = "day";
+        dayListItem.innerText = day;
+    
+        if (day === 24 || day === 25 || day === 31) {
+          dayListItem.className += " holiday";
+          if (day === 25) {
+            dayListItem.className += " friday";
+          }
+        } else if (day === 4 || day === 11 || day === 18) {
+          dayListItem.className += " friday";
+        }
+    
+        monthDaysList.appendChild(dayListItem);
       }
-    } else if (day === 4 || day === 11 || day === 18) {
-      dayListItem.className += " friday";
-    }
+}
 
-    monthDaysList.appendChild(dayListItem);
-  }
+function createHolydayBtn(holidays){
+    let holidayBtn = document.createElement('button')
+    holidayBtn.id = 'btn-holiday';
+    holidayBtn.innerText= holidays;
+
+    document.querySelector('.buttons-container').appendChild(holidayBtn);
+
+    
+
 }
 
 createDaysOfTheWeek();
+createDaysOfTheMonth();
+createHolydayBtn("Feriados");
+
 
 // Escreva seu código abaixo.
